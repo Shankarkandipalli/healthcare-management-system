47b5edcfeb01dd9f500156fe8e4a48d8d565f2f7
Shankarkandipalli <kandipalliishankar@gmail.com>
2026-08-04 18:19:24 +0530

    configure files added

---
+++ src/test/java/com/healthcare_management_system/SpringbootProjectForHealthcareManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProjectForHealthcareManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
